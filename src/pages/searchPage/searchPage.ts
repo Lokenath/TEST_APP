6b2e598db74fa5321d8ba3c50a09002405abf61f
lsingh3 <lokenathsingh@yahoo.com>
2017-11-01 20:27:26 +0530

    Left menu completed

--- src/pages/searchPage/searchPage.ts
+++ src/pages/searchPage/searchPage.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Page2 } from '../page2/page2';
+import { SearchResultPage } from '../searchResultPage/searchResultPage';
 
 @Component({
   selector: 'page-SearchPage',
   templateUrl: 'SearchPage.html'
 })
 export class SearchPage {
+  searchedPnr : any;
+  disabled : boolean;
 
   constructor(private navCtrl: NavController, navParams: NavParams) {
-    
+     this.searchedPnr = "10";
+     this.disabled = true;
+  }
+  onPnrType(ev){
+      if(ev.target.value.length == 10){
+          this.disabled = false;
+      }
+      else{
+          this.disabled = true;
+      }
   }
   navigateToResultPage(){
-      this.navCtrl.push(Page2, {});
+      this.navCtrl.push(SearchResultPage, {pnr: this.searchedPnr});
   }
 }
